1e7f6faa6ad4ebc1f4cfca1006d3bd8b3f49f5d0
Levi Senft <lsenft@gmail.com>
2015-06-28 13:56:10 -0600

    Add gantt config, required to render gantt charts.

--- book/plugin.js
+++ book/plugin.js
@@ -1,5 +1,13 @@
 require(["gitbook"], function(gitbook) {
   gitbook.events.bind("page.change", function() {
+    mermaid.ganttConfig = {
+        "titleTopMargin": 25,
+        "barHeight": 20,
+        "barGap": 4,
+        "topPadding": 75,
+        "sidePadding": 75
+      };
+
     mermaid.init();
   });
 });
